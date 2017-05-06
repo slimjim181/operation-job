1af044a7b083f539f08bae28aee76200d854ffbf
Joseph Sutton <josephsutton@hotmail.co.nz>
2017-05-06 16:24:14 +1200

    Setup eslint
    And some unit test setup (not all)
    Spelling mistake in readme

--- test/App.test.js
+++ test/App.test.js
@@ -1,8 +1,12 @@
+/* global document describe it */ // TODO fix with config
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import OperationJob from '../src/operation-job/operation-job-app';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<OperationJob />, div);
+describe('operation-job-app', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<OperationJob />, div);
+  });
 });
